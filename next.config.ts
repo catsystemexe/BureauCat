100abb9928671717c798714a3ca25350ffdf4bc8
catsystemexe <238760990+catsystemexe@users.noreply.github.com>
2026-06-05 14:58:13 +0000

    run

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "33eea652-88c8-4b79-8c9f-abc12090e8e8-00-2kyjoy66q73gy.riker.replit.dev"
+  ]
+};
 
 export default nextConfig;
